bab9d6c1f0d8d8b44975045c62ce240ebca4ef2b
Meghal Bothra <141125897+meghalbothra@users.noreply.github.com>
2024-07-12 18:13:20 +0530

    Update birdscript.cs

--- Assets/birdscript.cs
+++ Assets/birdscript.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class birdscript : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     public Rigidbody2D myRigidBody;
     public float flapStrength = 1000f; // Adjust this value to change the flap strength
     public LogicScript logic;
     public bool birdIsAlive = true;
     public float gameSpeed = 10.0f; // Adjust this value to change the game speed
+
     void Start()
     {
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
         Time.timeScale = gameSpeed; // Set the initial game speed
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && birdIsAlive) 
+        // Check for input on both PC and mobile devices
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0) && birdIsAlive)
         {
-            myRigidBody.velocity = Vector2.up * flapStrength;
+            // PC input or touch input
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                myRigidBody.velocity = Vector2.up * flapStrength;
+            }
+            else if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                myRigidBody.velocity = Vector2.up * flapStrength;
+            }
         }
     }
 
